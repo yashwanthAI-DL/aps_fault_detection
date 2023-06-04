b8855968cc7cce7af86a42888e15cbc6864aa97c
Yashwanth <cpyashwanthcaptainlevi@gmail.com>
2023-06-04 13:18:51 +0530

    updated requirements and setup reading requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+'''convert code into library we need setup.py'''
+from setuptools import find_namespace_packages,setup
+
+from typing import List
+REQUIRIMENT_file_name="requirements.txt"
+def get_requirements()-> List[str]:
+    with open(REQUIRIMENT_file_name) as requirement_file:
+        requirement_list=requirement_file.readlines()
+    requirement_list=[requirement_name.replace("\n","")for requirement_name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
+setup(
+    name='sensor',
+    version="0.0.1",
+    author = "yash",
+    author_email="cpyashwanth1ga17cs035@gmail.com",
+    install_packages=find_packagaes(),
+    install_requires=get_requirements(),
+)
