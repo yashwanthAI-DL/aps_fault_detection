84fbe492f85adb814ccf78900b684a571d23e2aa
Yashwanth <cpyashwanthcaptainlevi@gmail.com>
2023-06-04 13:32:33 +0530

    updated setup files

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 
 '''convert code into library we need setup.py'''
-from setuptools import find_namespace_packages,setup
+from setuptools import find_packages,setup
 
 from typing import List
 REQUIRIMENT_file_name="requirements.txt"
+HYPHEN_E_DOT="-e ."
+
 def get_requirements()-> List[str]:
     with open(REQUIRIMENT_file_name) as requirement_file:
         requirement_list=requirement_file.readlines()
     requirement_list=[requirement_name.replace("\n","")for requirement_name in requirement_list]
+
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
@@ -18,6 +21,6 @@ setup(
     version="0.0.1",
     author = "yash",
     author_email="cpyashwanth1ga17cs035@gmail.com",
-    install_packages=find_packagaes(),
+    install_packages=find_packages(),
     install_requires=get_requirements(),
 )
